2a611d2b865e00e1bab93ebf4d3b7a23232057cd
AbdullahIbrahim23 <AbdullahIbrahim23@users.noreply.github.com>
2017-11-14 13:37:05 -0500

    Day 1

---
+++ sketch.js
@@ -0,0 +1,54 @@
+let plat;
+let hero;
+
+function setup(){
+	createCanvas(400,400);
+	let x_ = 100;
+	let y_ = 100;
+	let width = 150;
+	plat = new Platform(x_, y_, width);
+	let x = 10;
+	let y = 40;
+	let xv = 0;
+	let yv = 0;
+	hero = new Hero(x, y, xv, yv);
+}
+
+function draw(){
+	plat.show();
+	hero.show();
+}
+
+class Hero{
+	constructor(x, y, xv, yv){
+		this.x = x;
+		this.y = y;
+		this.xvelocity = xv;
+		this.yvelocity = yv;
+		this.height = 35;
+		this.width = 20;
+	}
+	
+	show() {
+		stroke(255);
+		strokeWeight(4);
+		fill(0);
+		ellipse(this.x, this.y, this.width, this.height);
+	}
+}
+
+class Platform{
+	constructor(x_, y_, width){
+		this.x = x_;
+		this.y = y_;
+		this.width = width;
+		this.height = 20;
+	}
+	
+	show() {
+		stroke(255);
+		strokeWeight(4);
+		fill(155);
+		rect(this.x, this.y, this.width, this.height);
+	}
+}
